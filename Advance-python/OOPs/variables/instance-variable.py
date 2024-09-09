0cb8a7d3c5c8b73de33595795c8c84ba04711fa1
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2024-09-09 21:34:00 +0530

    Update var name instance-variable.py

--- Advance-python/OOPs/variables/instance-variable.py
+++ Advance-python/OOPs/variables/instance-variable.py
@@ -6,7 +6,7 @@ class MyClass:
         print(self.name)
 
 obj1 = MyClass("Harsh")
-obj2 = MyClass("Priyanshi")
+obj2 = MyClass("PS")
 
 obj1.print_name() # Output: John
 obj2.print_name() # Output: Jane
